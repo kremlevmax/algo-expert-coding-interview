f1c2c00abf46456ae3f98ccd90b51333e1c820da
kremlevmax <kremlevmax.webdev@icloud.com>
2023-02-04 23:47:12 -0800

    first commit

---
+++ Arrays/two-number-sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class TwoNumbersSum {
+    private static int[] sum(int[] numbersArray, int neededSum) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
