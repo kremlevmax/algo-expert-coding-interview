76c55e1d0218a317d310b3e8d9f563fb56da7d21
kremlevmax <kremlevmax.webdev@icloud.com>
2023-02-05 23:25:29 -0800

    first commit

---
+++ Arrays/TwoNumbersSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TwoNumbersSum {
+    private static int[] sumNaive(int[] numbersArray, int neededSum) {
+        for (int i = 0; i < numbersArray.length; i++) {
+            for (int j = 0; j < numbersArray.length; j++) {
+                if ((i != j) && numbersArray[i] + numbersArray[j] == neededSum) {
+                    return new int[]{numbersArray[i], numbersArray[j]};
+                }
+            }
+        }
+        return new int[2];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumNaive(new int[]{1, 2, 5, 9, 11, -3, 4}, 6)[1]);
+    }
+}
