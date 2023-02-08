6c4b4806c92d21aa2d10ba7c9692a4f6b2ca2029
kremlevmax <kremlevmax.webdev@icloud.com>
2023-02-07 22:46:22 -0800

    Two Numbers Sum 3 Solutions

--- Arrays/TwoNumbersSum.java
+++ Arrays/TwoNumbersSum.java
@@ -12,11 +12,40 @@ public class TwoNumbersSum {
         return new int[2];
     }
 
-    private static int[] sumSet(int[] numbers) {
-    
+    private static int[] sumSet(int[] numbers, int neededSum) {
+        Set<Integer> arrayCopy = new HashSet<>();
+        for (int number: numbers) {
+            int searchedNumber = neededSum - number;
+
+            if (arrayCopy.contains(searchedNumber)) {
+                return new int[]{number, searchedNumber};
+            } else {
+                arrayCopy.add(number);
+            }
+        }
+        return new int[]{};
+    }
+
+    private static int[] sumLeftRight(int[] numbers, int neededSum){
+        Arrays.sort(numbers);
+        int leftIndex = 0;
+        int rightIndex = numbers.length-1;
+
+        while (leftIndex < rightIndex) {
+            int currentSum = numbers[leftIndex]+numbers[rightIndex];
+            if (currentSum == neededSum) {
+                return new int[]{numbers[leftIndex], numbers[rightIndex]};
+            } else if (currentSum < neededSum) {
+                leftIndex += 1;
+            } else {
+                rightIndex -= 1;
+            }
+        }
+
+        return new int[]{};
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sumNaive(new int[]{1, 2, 5, 9, 11, -3, 4}, 6)[1]));
+        System.out.println(Arrays.toString(sumLeftRight(new int[]{1, 2, 5, 9, 11, -3, 4}, 20)));
     }
 }
