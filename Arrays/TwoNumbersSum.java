51aceadec89b8ad0c1c519b078d725db7c59e795
kremlevmax <kremlevmax.webdev@icloud.com>
2023-02-08 22:49:04 -0800

    Validate Subsequence start

--- Arrays/TwoNumbersSum.java
+++ Arrays/TwoNumbersSum.java
@@ -5,7 +5,7 @@ public class TwoNumbersSum {
         for (int i = 0; i < numbersArray.length; i++) {
             for (int j = 0; j < numbersArray.length; j++) {
                 if ((i != j) && numbersArray[i] + numbersArray[j] == neededSum) {
-                    return new int[]{numbersArray[i], numbersArray[j]};
+                    return new int[]{i, j};
                 }
             }
         }
@@ -46,6 +46,6 @@ public class TwoNumbersSum {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sumLeftRight(new int[]{1, 2, 5, 9, 11, -3, 4}, 20)));
+        System.out.println(Arrays.toString(sumNaive(new int[]{1, 2, 5, 9, 11, -3, 4}, 20)));
     }
 }
